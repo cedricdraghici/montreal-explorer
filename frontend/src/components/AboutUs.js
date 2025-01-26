dfbcff27d2f9c6bf83fd51c943044eb64ff0e040
Sara Yang <sarayang.rh@gmail.com>
2025-01-26 11:46:14 -0500

    intro malachi fuka

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -20,11 +20,11 @@ function AboutUs() {
             French, and English, and with growing proficiency in Korean and Spanish, I’m deeply inspired by\
              multicultural environments and the intersection of language and AI. Beyond academics, my interests \
              include reading movie critiques and staying active through skiing, biking, and running." },
-        { name: "Fuka", intro: "" },
+        { name: "Fuka", intro: "I’m Fuka Ono Computer Science and Honours Economics major at McGill. I have a strong passion for Data Analysis, Data Science, and Machine Learning, and I love creating projects that allow me to be both creative and impactful.What excites me most is using my skills to solve real-world problems and help others through innovative solutions. I am thrilled to collaborate, learn, and build something meaningful with teammates at hackathon." },
         { name: "Sara", intro: "Hi, I’m Sara. I'm majoring in CS. For this website, I collaborated with Cedric on \
             the front-end development, which has been an exciting learning experience. Outside of coding, I enjoy skateboarding, \
             archery, and exploring new hobbies that keep life adventurous and fun!" },
-        { name: "Malachi", intro: "" },
+        { name: "Malachi", intro: "Hi I'm Malachi, a CS and Linguistics student at McGill. This is my first time participating in a hackathon so excuse our half finished project. My personal passions lie in low-level and systems development. Rushed introduction ten minutes left :)" },
       ],
     },
     fr: {
@@ -35,11 +35,11 @@ function AboutUs() {
             le français et l'anglais, et je progresse en coréen et en espagnol. Je suis profondément inspirée par les \
             environnements multiculturels et l'intersection entre les langues et l'intelligence artificielle. En dehors de \
             mes études, mes intérêts incluent la lecture de critiques de films et les activités sportives comme le ski, le vélo et la course à pied." },
-        { name: "Fuka", intro: "" },
+        { name: "Fuka", intro: "Je m’appelle Fuka Ono, je suis en double majeure en Informatique et en Économie Honours à McGill. J’ai une grande passion pour l’analyse de données, la science des données et l’apprentissage automatique, et j’adore créer des projets qui me permettent d’être à la fois créative et d’avoir un impact. Ce qui m’enthousiasme le plus, c’est d’utiliser mes compétences pour résoudre des problèmes concrets et aider les autres grâce à des solutions innovantes. Je suis ravie de collaborer, d’apprendre et de construire quelque chose de significatif avec mes coéquipiers lors de ce hackathon." },
         { name: "Sara", intro: "Allô, je m'appelle Sara. Je suis étudiante en informatique. Pour ce site web, j'ai collaboré \
             avec Cedric sur le développement front-end, ce qui a été une expérience d'apprentissage passionnante. En dehors du \
             codage, j'aime faire du skateboard, du tir à l'arc et explorer de nouveaux passe-temps qui rendent la vie aventureuse et amusante !" },
-        { name: "Malachi", intro: "" },
+        { name: "Malachi", intro: "Salut, je m'appelle Malachi, je suis étudiant en Informatique et en Linguistique à McGill. C'est ma première participation à un hackathon, alors excusez notre projet à moitié terminé. Mes passions personnelles se trouvent dans le développement bas niveau et les systèmes. Introduction rapide, il reste dix minutes :)" },
       ],
     },
   };
